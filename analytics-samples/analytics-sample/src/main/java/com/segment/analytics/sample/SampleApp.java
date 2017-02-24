730d7f0544ab77e1e12370c37ac45e7ca52bb9fc
Prateek Srivastava <f2prateek@gmail.com>
2017-02-24 15:57:32 -0800

    Add Middlewares

--- analytics-samples/analytics-sample/src/main/java/com/segment/analytics/sample/SampleApp.java
+++ analytics-samples/analytics-sample/src/main/java/com/segment/analytics/sample/SampleApp.java
@@ -3,6 +3,8 @@ package com.segment.analytics.sample;
 import android.app.Application;
 import android.util.Log;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.Middleware;
+import com.segment.analytics.integrations.BasePayload;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class SampleApp extends Application {
@@ -25,7 +27,15 @@ public class SampleApp extends Application {
         new Analytics.Builder(this, ANALYTICS_WRITE_KEY)
             .trackApplicationLifecycleEvents()
             .trackAttributionInformation()
-            .recordScreenViews();
+            .recordScreenViews()
+            .interceptor(
+                new Middleware() {
+                  @Override
+                  public void intercept(Chain chain) {
+                    BasePayload payload = chain.payload();
+                    // todo: builder types.
+                  }
+                });
 
     // Set the initialized instance as a globally accessible instance.
     Analytics.setSingletonInstance(builder.build());
